691e9986fc14d720e1cfcc27d94e95585c3c7e4c
andersonaguia <andersonlaguiar@gmail.com>
2023-10-21 12:23:39 -0300

    reconfigura o TypeORM

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import configuration from 'src/common/env';
+import { join } from 'path';
+import { dataSourceOptions } from 'src/core/database/data-source';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', '..', 'public'),
+    }),
+    TypeOrmModule.forRoot({ autoLoadEntities: true, ...dataSourceOptions }),
+  ],
   controllers: [],
   providers: [],
 })
