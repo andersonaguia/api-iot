46cec405e9f461f3ba9e1ff16777a0a10744e07c
andersonaguia <andersonlaguiar@gmail.com>
2023-10-25 13:22:26 -0300

    refactor: traduz as respostas http do auth controller

--- src/core/auth/controllers/auth.controller.ts
+++ src/core/auth/controllers/auth.controller.ts
@@ -1,123 +1,126 @@
 import {
-    Body,
-    Controller,
-    HttpStatus,
-    Patch,
-    Post,
-    UseGuards,
-    ValidationPipe,
-  } from '@nestjs/common';
-  import { isNumber } from 'class-validator';
-  import { NestResponseBuilder } from 'src/core/http/nest-response-builder';
-  import { AuthService } from '../services/auth.service';
-  import { ChangePasswordDTO } from '../dto/change-password.dto';
-  import { CredentialsDTO } from '../dto/credentials.dto';
-  import { JwtAuthGuard } from '../guards/jwt-auth.guard';
-  import { CreateUserDto } from 'src/modules/users/dto/create-user.dto';
+  Body,
+  Controller,
+  HttpStatus,
+  Patch,
+  Post,
+  UseGuards,
+  ValidationPipe,
+} from "@nestjs/common";
+import { isNumber } from "class-validator";
+import { NestResponseBuilder } from "src/core/http/nest-response-builder";
+import { AuthService } from "../services/auth.service";
+import { ChangePasswordDTO } from "../dto/change-password.dto";
+import { CredentialsDTO } from "../dto/credentials.dto";
+import { JwtAuthGuard } from "../guards/jwt-auth.guard";
+import { CreateUserDto } from "src/modules/users/dto/create-user.dto";
 
-  @Controller("auth")
-  export class AuthController {
-    constructor(private readonly authService: AuthService) {}
-  
-    @Post('/signup')
-    async signUp(@Body(ValidationPipe) createUserDto: CreateUserDto) {
-      try {
-        const result = await this.authService.signUp(createUserDto);
-        if (result === null) {
-          return new NestResponseBuilder()
-            .withStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-            .withBody({
-              statusCode: HttpStatus.UNPROCESSABLE_ENTITY,
-              message: 'Passwords do not match',
-            })
-            .build();
-        } else if (result.id) {
-          return new NestResponseBuilder()
-            .withStatus(HttpStatus.CREATED)
-            .withBody({
-              statusCode: HttpStatus.CREATED,
-              message: 'Successful registration',
-            })
-            .build();
-        }
-      } catch (error) {
-        console.log(error);
-        if (error.code === 'ER_DUP_ENTRY') {
-          return new NestResponseBuilder()
-            .withStatus(HttpStatus.CONFLICT)
-            .withBody({
-              code: 409,
-              detail: "Existe um usuário cadastrado com o mesmo e-mail",
-            })
-            .build();
-        }
+@Controller("auth")
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post("/signup")
+  async signUp(@Body(ValidationPipe) createUserDto: CreateUserDto) {
+    try {
+      const result = await this.authService.signUp(createUserDto);
+      if (result === null) {
         return new NestResponseBuilder()
-          .withStatus(HttpStatus.BAD_REQUEST)
+          .withStatus(HttpStatus.UNPROCESSABLE_ENTITY)
           .withBody({
-            code: error.code,
-            detail: error.sqlMessage,
+            statusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+            message: "Senhas não conferem",
+          })
+          .build();
+      } else if (result.id) {
+        return new NestResponseBuilder()
+          .withStatus(HttpStatus.CREATED)
+          .withBody({
+            statusCode: HttpStatus.CREATED,
+            message: "Usuário cadastrado com sucesso!",
           })
           .build();
       }
-    }
-  
-    @Post('/signin')
-    async signIn(
-      @Body(ValidationPipe)
-      credentialsDto: CredentialsDTO,
-    ) {
-      const result = await this.authService.signIn(credentialsDto);
-      if (result === null) {
+    } catch (error) {
+      console.log(error);
+      if (error.code === "ER_DUP_ENTRY") {
         return new NestResponseBuilder()
-          .withStatus(HttpStatus.UNAUTHORIZED)
+          .withStatus(HttpStatus.CONFLICT)
           .withBody({
-            statusCode: HttpStatus.UNAUTHORIZED,
-            message: 'Incorrect email or password',
+            statusCode: 409,
+            detail: "Existe um usuário cadastrado com o mesmo e-mail",
           })
-          .build();
-      } else if (result.token) {
-        return new NestResponseBuilder()
-          .withStatus(HttpStatus.OK)
-          .withBody(result)
           .build();
       }
       return new NestResponseBuilder()
         .withStatus(HttpStatus.BAD_REQUEST)
-        .withBody(result)
-        .build();
-    }
-  
-    @UseGuards(JwtAuthGuard)
-    @Patch('/changepassword')
-    async changePassword(@Body() data: ChangePasswordDTO) {
-      const result = await this.authService.changePassword(data);
-      if (result === null) {
-        return new NestResponseBuilder()
-          .withStatus(HttpStatus.UNAUTHORIZED)
-          .withBody({
-            statusCode: HttpStatus.UNAUTHORIZED,
-            message: 'Incorrect email or oldPassword',
-          })
-          .build();
-      } else if (isNumber(result)) {
-        if (result > 0) {
-          return new NestResponseBuilder()
-            .withStatus(HttpStatus.OK)
-            .withBody('Password changed successfully')
-            .build();
-        } else {
-          return new NestResponseBuilder()
-            .withStatus(HttpStatus.NOT_FOUND)
-            .withBody({
-              code: 20000,
-              detail: 'This id not found or unable to update',
-            })
-            .build();
-        }
-      }
-      return new NestResponseBuilder()
-        .withStatus(HttpStatus.BAD_REQUEST)
-        .withBody(result)
+        .withBody({
+          statusCode: error.code,
+          detail: error.sqlMessage,
+        })
         .build();
     }
   }
+
+  @Post("/signin")
+  async signIn(
+    @Body(ValidationPipe)
+    credentialsDto: CredentialsDTO
+  ) {
+    const result = await this.authService.signIn(credentialsDto);
+    if (result === null) {
+      return new NestResponseBuilder()
+        .withStatus(HttpStatus.UNAUTHORIZED)
+        .withBody({
+          statusCode: HttpStatus.UNAUTHORIZED,
+          message: "Usuário ou senha inválidos!",
+        })
+        .build();
+    } else if (result.token) {
+      return new NestResponseBuilder()
+        .withStatus(HttpStatus.OK)
+        .withBody(result)
+        .build();
+    }
+    return new NestResponseBuilder()
+      .withStatus(HttpStatus.BAD_REQUEST)
+      .withBody(result)
+      .build();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch("/changepassword")
+  async changePassword(@Body() data: ChangePasswordDTO) {
+    const result = await this.authService.changePassword(data);
+    if (result === null) {
+      return new NestResponseBuilder()
+        .withStatus(HttpStatus.UNAUTHORIZED)
+        .withBody({
+          statusCode: HttpStatus.UNAUTHORIZED,
+          message: "E-mail ou senha antiga incorretos!",
+        })
+        .build();
+    } else if (isNumber(result)) {
+      if (result > 0) {
+        return new NestResponseBuilder()
+          .withStatus(HttpStatus.OK)
+          .withBody({
+            statusCode: HttpStatus.OK,
+            message: "Senha alterada com sucesso!",
+          })
+          .build();
+      } else {
+        return new NestResponseBuilder()
+          .withStatus(HttpStatus.NOT_FOUND)
+          .withBody({
+            statusCode: 404,
+            detail: "Id não encontrado ou indisponível para atualização",
+          })
+          .build();
+      }
+    }
+    return new NestResponseBuilder()
+      .withStatus(HttpStatus.BAD_REQUEST)
+      .withBody(result)
+      .build();
+  }
+}
