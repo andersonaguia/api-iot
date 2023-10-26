9eaf98be5e47d4d108b1cf2e43a37fd880a79a76
andersonaguia <andersonlaguiar@gmail.com>
2023-10-26 20:11:41 -0300

    corrige as migrations

--- src/core/database/migrations/1698335295637-thermistors.ts
+++ src/core/database/migrations/1698335295637-thermistors.ts
@@ -6,11 +6,9 @@ export class thermistors1698335295637 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE \`thermistors\` DROP COLUMN \`value\``);
         await queryRunner.query(`ALTER TABLE \`thermistors\` ADD UNIQUE INDEX \`IDX_a576d99c2d2400bad7780d40d0\` (\`serialNumber\`)`);
-        await queryRunner.query(`ALTER TABLE \`thermistors\` ADD UNIQUE INDEX \`IDX_a576d99c2d2400bad7780d40d0\` (\`serialNumber\`)`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`thermistors\` DROP INDEX \`IDX_a576d99c2d2400bad7780d40d0\``);
         await queryRunner.query(`ALTER TABLE \`thermistors\` DROP INDEX \`IDX_a576d99c2d2400bad7780d40d0\``);
         await queryRunner.query(`ALTER TABLE \`thermistors\` ADD \`value\` decimal NOT NULL`);
     }
