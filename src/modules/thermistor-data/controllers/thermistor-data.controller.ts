009a428ba5bdcde6e4d01cc6e199fdc9be8b0ae6
andersonaguia <andersonlaguiar@gmail.com>
2023-10-27 08:36:34 -0300

    cria rota para utilização do metodo

--- src/modules/thermistor-data/controllers/thermistor-data.controller.ts
+++ src/modules/thermistor-data/controllers/thermistor-data.controller.ts
@@ -1,7 +1,17 @@
-import { Body, Controller, Get, HttpStatus, Param, Post } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Query,
+} from "@nestjs/common";
 import { ThermistorDataService } from "../services/thermistor-data.service";
 import { CreateThermistorDataDto } from "../dto/create-thermistor-data.dto";
 import { NestResponseBuilder } from "src/core/http/nest-response-builder";
+import { FindValuesByDateDto } from "../dto/find-by-date.dto";
 
 @Controller("/thermistordata")
 export class ThermistorDataController {
@@ -54,6 +64,53 @@ export class ThermistorDataController {
     try {
       const result = await this.thermistorDataService.findActualValue(
         thermistorId
+      );
+
+      return new NestResponseBuilder()
+        .withStatus(HttpStatus.OK)
+        .withBody({
+          statusCode: HttpStatus.OK,
+          data: result,
+        })
+        .build();
+    } catch (error) {
+      if (error.code === 404) {
+        return new NestResponseBuilder()
+          .withStatus(HttpStatus.NOT_FOUND)
+          .withBody({
+            statusCode: HttpStatus.NOT_FOUND,
+            detail: error.message,
+          })
+          .build();
+      }
+      return new NestResponseBuilder()
+        .withStatus(HttpStatus.BAD_REQUEST)
+        .withBody({
+          statusCode: HttpStatus.BAD_REQUEST,
+          detail: error.sqlMessage,
+        })
+        .build();
+    }
+  }
+
+  @Get("/find-values-by-date")
+  async findValuesByDate(
+    @Query("thermistorId") thermistorId: number,
+    @Query("startDate") startDate: Date,
+    @Query("endDate") endDate: Date,
+    @Query("page", new DefaultValuePipe(1)) page: number,
+    @Query("limit", new DefaultValuePipe(10)) limit: number
+  ) {
+    try {
+      const searchData = new FindValuesByDateDto();
+      searchData.thermistorId = +thermistorId;
+      searchData.startDate = startDate;
+      searchData.endDate = endDate;
+      searchData.page = page;
+      searchData.limit = limit;
+
+      const result = await this.thermistorDataService.findValuesByDate(
+        searchData
       );
 
       return new NestResponseBuilder()
