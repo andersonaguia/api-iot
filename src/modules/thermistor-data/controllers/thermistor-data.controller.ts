8127063781195147dce5bc552b1e3cc0b107dbde
andersonaguia <andersonlaguiar@gmail.com>
2023-10-26 21:35:15 -0300

    cria rota find actual value

--- src/modules/thermistor-data/controllers/thermistor-data.controller.ts
+++ src/modules/thermistor-data/controllers/thermistor-data.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpStatus, Post } from "@nestjs/common";
+import { Body, Controller, Get, HttpStatus, Param, Post } from "@nestjs/common";
 import { ThermistorDataService } from "../services/thermistor-data.service";
 import { CreateThermistorDataDto } from "../dto/create-thermistor-data.dto";
 import { NestResponseBuilder } from "src/core/http/nest-response-builder";
@@ -44,6 +44,40 @@ export class ThermistorDataController {
         .withBody({
           statusCode: error.code,
           message: error.sqlMessage,
+        })
+        .build();
+    }
+  }
+
+  @Get("/findactualvalue/:thermistorid")
+  async findBySerialNumber(@Param("thermistorid") thermistorId: number) {
+    try {
+      const result = await this.thermistorDataService.findActualValue(
+        thermistorId
+      );
+
+      return new NestResponseBuilder()
+        .withStatus(HttpStatus.OK)
+        .withBody({
+          statusCode: HttpStatus.OK,
+          data: result,
+        })
+        .build();
+    } catch (error) {
+      if (error.code === 404) {
+        return new NestResponseBuilder()
+          .withStatus(HttpStatus.NOT_FOUND)
+          .withBody({
+            statusCode: HttpStatus.NOT_FOUND,
+            detail: error.message,
+          })
+          .build();
+      }
+      return new NestResponseBuilder()
+        .withStatus(HttpStatus.BAD_REQUEST)
+        .withBody({
+          statusCode: HttpStatus.BAD_REQUEST,
+          detail: error.sqlMessage,
         })
         .build();
     }
