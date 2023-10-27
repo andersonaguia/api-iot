6469d503625da032fca3b0bdf0a54fc1ee1b8e00
andersonaguia <andersonlaguiar@gmail.com>
2023-10-27 08:34:32 -0300

    adiciona metodo findValuesByDate ao service

--- src/modules/thermistor-data/services/thermistor-data.service.ts
+++ src/modules/thermistor-data/services/thermistor-data.service.ts
@@ -3,6 +3,7 @@ import { ThermistorsDataRepository } from "../thermistor-data.repository";
 import { ThermistorsService } from "src/modules/thermistors/services/thermistors.service";
 import { CreateThermistorDataDto } from "../dto/create-thermistor-data.dto";
 import { ThermistorDataEntity } from "../entities/thermistor-data.entity";
+import { FindValuesByDateDto } from "../dto/find-by-date.dto";
 
 @Injectable()
 export class ThermistorDataService {
@@ -56,6 +57,30 @@ export class ThermistorDataService {
             await this.thermistorsDataRepository.findActualValue(thermistorId);
 
           resolve(thermistorData);
+        }
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  findValuesByDate(data: FindValuesByDateDto): Promise<any> {
+    return new Promise(async (resolve, reject) => {
+      const { thermistorId } = data;
+      try {
+        const thermistor = await this.thermistorsService.findById(
+          +thermistorId
+        );
+
+        if (!thermistor) {
+          reject({
+            code: 404,
+            message: "Nenhum termistor encontrado para o id informado",
+          });
+        } else {
+          const thermistorValues =
+            await this.thermistorsDataRepository.findValuesByDate(data);
+          resolve(thermistorValues);
         }
       } catch (error) {
         reject(error);
