3888dcc15fcc2e98180dd4eed18cd2677a650f13
andersonaguia <andersonlaguiar@gmail.com>
2023-10-27 10:14:05 -0300

    refactor: adiciona somente leitura aos itens

--- src/modules/thermistors/dto/thermistor-value.dto.ts
+++ src/modules/thermistors/dto/thermistor-value.dto.ts
@@ -3,44 +3,44 @@ import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 export class ThermistorValueDto {
   @IsNotEmpty({ message: "Campo manufacturer não pode ser vazio" })
   @IsString({ message: "Campo manufacturer deve ser uma string" })
-  manufacturer: string;
+  readonly manufacturer: string;
 
   @IsNotEmpty({ message: "Campo model não pode ser vazio" })
   @IsString({ message: "Campo model deve ser uma string" })
-  model: string;
+  readonly model: string;
 
   @IsNotEmpty({ message: "Campo minRange não pode ser vazio" })
   @IsNumber({}, { message: "Campo minRange deve ser uma string" })
-  minRange: number;
+  readonly minRange: number;
 
   @IsNotEmpty({ message: "Campo maxRange não pode ser vazio" })
   @IsNumber({}, { message: "Campo maxRange deve ser uma string" })
-  maxRange: number;
+  readonly maxRange: number;
 
   @IsNotEmpty({ message: "Campo nominalResistance não pode ser vazio" })
   @IsNumber({}, { message: "Campo nominalResistance deve ser uma string" })
-  nominalResistance: number;
+  readonly nominalResistance: number;
 
   @IsNotEmpty({ message: "Campo voltageDividerResistance não pode ser vazio" })
   @IsNumber(
     {},
     { message: "Campo voltageDividerResistance deve ser uma string" }
   )
-  voltageDividerResistance: number;
+  readonly voltageDividerResistance: number;
 
   @IsNotEmpty({ message: "Campo serialNumber não pode ser vazio" })
   @IsString({ message: "Campo serialNumber deve ser uma string" })
-  serialNumber: string;
+  readonly serialNumber: string;
 
   @IsNotEmpty({ message: "Campo controllerId não pode ser vazio" })
   @IsNumber({}, { message: "Campo controllerId deve ser uma string" })
-  controllerId: number;
+  readonly controllerId: number;
 
   @IsNotEmpty({ message: "Campo controllerPort não pode ser vazio" })
   @IsString({ message: "Campo controllerPort deve ser uma string" })
-  controllerPort: string;
+  readonly controllerPort: string;
 
   @IsNotEmpty({ message: "Campo location não pode ser vazio" })
   @IsString({ message: "Campo location deve ser uma string" })
-  location: string;
+  readonly location: string;
 }
