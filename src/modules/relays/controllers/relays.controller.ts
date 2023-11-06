0a3c26383bcdc2adee179cc458e417fe9f02efd6
andersonaguia <andersonlaguiar@gmail.com>
2023-11-06 17:39:57 -0300

    cria relays controller

---
+++ src/modules/relays/controllers/relays.controller.ts
@@ -0,0 +1,103 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Post,
+  Query,
+} from "@nestjs/common";
+import { NestResponseBuilder } from "src/core/http/nest-response-builder";
+import { RelaysService } from "../services/relays.service";
+import { CreateRelayDto } from "../dto/create-relay.dto";
+
+@Controller("relays")
+export class RelaysController {
+  constructor(private readonly relaysService: RelaysService) {}
+
+  @Post("/add")
+  async add(@Body() data: CreateRelayDto) {
+    try {
+      const result = await this.relaysService.addRelay(data);
+      if (result) {
+        return new NestResponseBuilder()
+          .withStatus(HttpStatus.CREATED)
+          .withBody({
+            statusCode: HttpStatus.CREATED,
+            message: "Relé cadastrado com sucesso",
+          })
+          .build();
+      } else {
+        return new NestResponseBuilder()
+          .withStatus(HttpStatus.BAD_REQUEST)
+          .withBody({
+            statusCode: HttpStatus.BAD_REQUEST,
+            message: "Falha ao cadastrar o relé. Tente novamente!",
+          })
+          .build();
+      }
+    } catch (error) {
+      if (error.code === 404) {
+        return new NestResponseBuilder()
+          .withStatus(HttpStatus.NOT_FOUND)
+          .withBody({
+            statusCode: error.code,
+            message: error.message,
+          })
+          .build();
+      } else if (error.code === 409) {
+        return new NestResponseBuilder()
+          .withStatus(HttpStatus.CONFLICT)
+          .withBody({
+            statusCode: error.code,
+            message: error.message,
+          })
+          .build();
+      }
+      return new NestResponseBuilder()
+        .withStatus(HttpStatus.BAD_REQUEST)
+        .withBody({
+          statusCode: error.code,
+          message: error.sqlMessage,
+        })
+        .build();
+    }
+  }
+
+  @Get("/findbycontrollerport/")
+  async findByControllerPort(
+    @Query("controllerId") controllerId: number,
+    @Query("controllerPort") controllerPort: number
+  ) {
+    try {
+      const result = await this.relaysService.findByControllerPort(
+        +controllerId,
+        +controllerPort
+      );
+
+      return new NestResponseBuilder()
+        .withStatus(HttpStatus.OK)
+        .withBody({
+          statusCode: HttpStatus.OK,
+          data: result,
+        })
+        .build();
+    } catch (error) {
+      if (error.code === 404) {
+        return new NestResponseBuilder()
+          .withStatus(HttpStatus.NOT_FOUND)
+          .withBody({
+            statusCode: HttpStatus.NOT_FOUND,
+            detail: error.message,
+          })
+          .build();
+      }
+      return new NestResponseBuilder()
+        .withStatus(HttpStatus.BAD_REQUEST)
+        .withBody({
+          statusCode: HttpStatus.BAD_REQUEST,
+          detail: error.sqlMessage,
+        })
+        .build();
+    }
+  }
+}
