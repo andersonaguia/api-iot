9daf3f01d0add57c0254dcd6714d353d998156d6
andersonaguia <andersonlaguiar@gmail.com>
2023-11-09 13:06:49 -0300

    modifica DTO para permitir criar dados

--- src/modules/relay-data/dto/new-relay-state.ts
+++ src/modules/relay-data/dto/new-relay-state.ts
@@ -3,13 +3,13 @@ import { IsBoolean, IsNotEmpty, IsNumber } from "class-validator";
 export class NewRelayStateDto {
   @IsNotEmpty({ message: "Obrigatório informar o campo expectedLevel" })
   @IsBoolean({ message: "Campo expectedLevel deve ser um booleano" })
-  readonly expectedLevel: boolean;
+  expectedLevel: boolean;
 
   @IsNotEmpty({ message: "Obrigatório informar o campo currentLevel" })
   @IsBoolean({ message: "Campo currentLevel deve ser um booleano" })
-  readonly currentLevel: boolean;
+  currentLevel: boolean;
 
   @IsNotEmpty({ message: "Obrigatório informar o campo relayId" })
   @IsNumber({}, { message: "Campo relayId deve ser um número" })
-  readonly relayId: number;
+  relayId: number;
 }
